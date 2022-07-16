d658d9595e2908c9a5dc166ddb3eb80a16842a51
Annie Bauer <overheads165@gmail.com>
2022-07-16 19:53:38 +0300

    for in

---
+++ src/js/app.js
@@ -0,0 +1,35 @@
+
+const object = {
+  name: "мечник",
+  health: 10,
+  level: 2,
+  attack: 80,
+  defence: 40,
+};
+
+const orderByProps = (obj, sortKeys) => {
+  var keys = [];
+  for (const prop in obj) {
+    if (obj.hasOwnProperty(prop)) {
+      keys.push(prop);
+    }
+  }
+  const tail = keys.filter((el) => !sortKeys.includes(el)).sort();
+  const result = sortKeys.concat(tail).map((prop) => {
+    return {
+      key: prop,
+      value: obj[prop],
+    };
+  });
+  return result
+};
+
+const result = orderByProps(object, ["name", "level"]);
+console.log(result); // ->
+//  [
+//    {key: "name", value: "мечник"}, // порядок взят из массива с ключами
+//    {key: "level", value: 2}, // порядок взят из массива с ключами
+//    {key: "attack", value: 80}, // порядок по алфавиту (т.к. в массиве с ключами нет значения "attack")
+//    {key: "defence", value: 40}, // порядок по алфавиту (т.к. в массиве с ключами нет значения "defence")
+//    {key: "health", value: 10} // порядок по алфавиту (т.к. в массиве с ключами нет значения "health")
+//  ]
